179e2cad729b135d348daa28f35328fedb53e4bb
hozhai <zhaihongmeng@gmail.com>
2026-04-11 19:36:48 -0400

    thank you for the hackathon, goodbye

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  nitro: {
+    preset: "vercel",
+  },
   css: ["~/assets/css/tailwind.css"],
 
   modules: [
